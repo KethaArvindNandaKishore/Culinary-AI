5c6a51ff7934c1cc987ee042373a22f01e7729e9
KETHA ARVIND  NANDA  KISHORE <arvindnandakishore@gmail.com>
2025-03-05 15:03:50 +0530

    Update chatBot.js

--- server/controllers/chatBot.js
+++ server/controllers/chatBot.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const ChatBot = async (req, res) => {
     let prompt = req.body.prompt;
-    const genAI = new GoogleGenerativeAI("AIzaSyBfhBUMgpNJW4tjlLOpJG1rATcG60GIGWc");
+    const genAI = new GoogleGenerativeAI("##################################");
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(prompt);
     console.log(result);
